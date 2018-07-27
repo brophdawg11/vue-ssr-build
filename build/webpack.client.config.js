3c1cc36a6ea1b4befb348ff453e936362fad88cf
Matt Brophy <matt@brophy.org>
2018-07-27 15:16:31 -0400

    Update to new server approach to fix HMR

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -9,8 +9,6 @@ const { isLocal, getBaseConfig } = require('./webpack.base.config');
 
 module.exports = function getClientConfig(rootDir) {
     const clientConfig = merge(getBaseConfig('client', rootDir), {
-        // Note: This name must begin with 'client' in order to be picked up by the
-        // webpack-hot-server-middleware plugin
         name: 'client',
         entry: [
             './src/js/entry-client.js',
@@ -24,16 +22,8 @@ module.exports = function getClientConfig(rootDir) {
     if (isLocal) {
         // Wire up HMR on the client
         // Can't use chunkhash while using HMR plugins
-        clientConfig.output.filename = '[name].[hash].js';
-        clientConfig.output.chunkFilename = '[name].chunk.[hash].js';
-        clientConfig.entry = [
-            'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
-            ...clientConfig.entry,
-        ];
-        clientConfig.plugins = [
-            new webpack.HotModuleReplacementPlugin(),
-            ...clientConfig.plugins,
-        ];
+        // clientConfig.output.filename = '[name].[hash].js';
+        // clientConfig.output.chunkFilename = '[name].chunk.[hash].js';
     }
 
     return clientConfig;
---
+++ src/renderer.js
@@ -0,0 +1,104 @@
+const path = require('path');
+const fs = require('fs');
+const LRU = require('lru-cache');
+const { createBundleRenderer } = require('vue-server-renderer');
+const expressVersion = require('express/package.json').version;
+const vsrVersion = require('vue-server-renderer/package.json').version;
+
+const { config } = require('../js/isomorphic-utils');
+const logger = require('../js/logger');
+
+const USE_STREAM = true;
+const serverInfo = `express/${expressVersion} vue-server-renderer/${vsrVersion}`;
+const resolve = file => path.resolve(__dirname, file);
+const templatePath = resolve('../index.tpl.html');
+
+let renderer;
+let readyPromise;
+
+function createRenderer(bundle, options) {
+    return createBundleRenderer(bundle, Object.assign(options, {
+        cache: LRU({
+            max: 1000,
+            maxAge: 1000 * 60 * 15,
+        }),
+        runInNewContext: false,
+    }));
+}
+
+const handleError = (err, res) => {
+    if (err.url) {
+        logger.debug('Redirecting from SSR error:', err.url);
+        res.redirect(err.url);
+    } else if (err.code === 404) {
+        res.status(err.code).send('404 | Page Not Found');
+    } else {
+        // Render Error Page or Redirect
+        res.status(500).send('500 | Internal Server Error');
+        logger.error(err);
+        logger.error(err.stack);
+    }
+};
+
+function renderToString(context, res) {
+    renderer.renderToString(context,
+        (err, html) => (err ? handleError(err, res) : res.end(html)),
+        e => handleError(e, res));
+}
+
+function renderToStream(context, res) {
+    const stream = renderer.renderToStream(context);
+    stream.on('data', data => res.write(data.toString()));
+    stream.on('end', () => res.end());
+    stream.on('error', err => handleError(err, res));
+}
+
+function render(req, res) {
+    const s = Date.now();
+
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Server', serverInfo);
+
+    const context = {
+        title: 'URBN Community',
+        req,
+        res,
+        url: req.url,
+        initialState: null,
+    };
+
+    // Render the appropriate Vue components into the renderer template
+    // using the server render logic in entry-server.js
+    if (USE_STREAM) {
+        renderToStream(context, res);
+    } else {
+        renderToString(context, res);
+    }
+
+    if (!config.isProd) {
+        logger.log(`Request took: ${Date.now() - s}ms`);
+    }
+}
+
+module.exports = function initVueRenderer(app) {
+    /* eslint-disable global-require, import/no-unresolved */
+    if (config.isLocal) {
+        // In development: setup the dev server with watch and hot-reload,
+        // and create a new renderer on bundle / index template update.
+        readyPromise = require('./setup-dev-server')(
+            app,
+            templatePath,
+            (bundle, options) => { renderer = createRenderer(bundle, options); },
+        );
+    } else {
+        const template = fs.readFileSync(templatePath, 'utf-8');
+        const bundle = require('../../dist/vue-ssr-server-bundle.json');
+        const clientManifest = require('../../dist/vue-ssr-client-manifest.json');
+        renderer = createRenderer(bundle, { template, clientManifest });
+    /* eslint-enable global-require, import/no-unresolved */
+
+    return config.isProd ? render : (req, res) => {
+        // Make dev server wait on webpack builds
+        readyPromise.then(() => render(req, res));
+    };
+};
