e935db8f8f2c95af05a7be84c4e64bec9e26aefb
Sean Kennedy <sean@codeserious.com>
2019-05-09 13:12:12 -0400

    WIP Adding Babel 7 support (#35)
    * Upgrading packages to babel7 and fixing issues from npm audit
    
    * Removing carets from package.json
    
    * Fixing missed babel7 migration locs
    
    * Removing stage 3, converting to features
    
    * Adding missing babel7 plugin req
    
    * Adding rest operator plugin
    
    * Converting spread operator to use loose to conform with v6 functionality
    
    * Converting to babel.config file
    
    * Updating plugins for eslint / vue
    
    * Upgrading jest and babel-jest
    
    * Adding babel core bridge
    
    * Upgrading jest
    
    * Adding option to remove comments
    
    * Reverting readme creation
    
    * Reverting source comment change
    
    * PR feedback
    
    * Adding babel merge
    
    * PR feedback

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+module.exports = (api) => {
+    api.cache(true);
+    return {
+        presets: [
+            ['@babel/preset-env', {
+                targets: {
+                    browsers: ['>5%', 'last 2 versions'],
+                },
+                modules: false,
+            }],
+        ],
+        plugins: [
+            // Allow proper tree shaking of lodash ES6 named imports
+            'lodash',
+            // We need loose due to new specifications in v7 https://babeljs.io/docs/en/v7-migration
+            ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
+            '@babel/plugin-syntax-dynamic-import',
+        ],
+        env: {
+            test: {
+                presets: [
+                    ['env', {
+                        modules: 'commonjs',
+                    }],
+                ],
+            },
+        },
+    };
+};
